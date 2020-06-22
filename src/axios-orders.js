5d46fae18a97fcb2a50571cd5bda67c0f7ebac80
JuliaDer <julia.dernovska@gmail.com>
2020-06-22 15:56:50 +0200

    Update file

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://my-burger-app-5b303.firebaseio.com/'
+  baseURL: 'https://react-my-burger-c67b2.firebaseio.com/'
 });
 
 export default instance;
