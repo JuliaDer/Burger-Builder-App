0a170688a48301aa5efcc45fee062a1b6d9a0dc7
JuliaDer <julia.dernovska@gmail.com>
2020-06-25 20:00:31 +0200

    Improving Project

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -54,7 +54,6 @@ export const auth = (email, password, isSignup) => {
     }
     axios.post(url, authData)
     .then(response => {
-      console.log(response)
       const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
       localStorage.setItem('token', response.data.idToken);
       localStorage.setItem('expirationDate', expirationDate);
@@ -63,7 +62,6 @@ export const auth = (email, password, isSignup) => {
       dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(err => {
-      console.log(err)
       dispatch(authFail(err.response.data.error));
     });
   };
@@ -82,7 +80,7 @@ export const authCheckState = () => {
     if (!token) {
       dispatch(logout());
     } else {
-      const expirationDate = localStorage.getItem('expirationDate');
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
       if (expirationDate < new Date()) {
         dispatch(logout());
       } else {
